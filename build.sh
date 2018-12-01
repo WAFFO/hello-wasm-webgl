b26e7d8c4e43585c4c2ba0d4e721e26a81ddf048
Garrett Steffen <garrettsteffen@gmail.com>
2018-12-01 13:31:23 -0600

    Added wasm-bindgen install to build steps.

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 set -ex
 cd "$(dirname $0)"
 
+if ( ! command -v wasm-bindgen )
+then
+    cargo install wasm-bindgen-cli
+fi
+
 cargo build --target wasm32-unknown-unknown
 
 wasm-bindgen ./target/wasm32-unknown-unknown/debug/hello_webgl.wasm --out-dir ./www/wasm
