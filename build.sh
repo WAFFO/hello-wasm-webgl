f9b8438adde4da2886b8f62a98282d037bf57678
Garrett Steffen <garrettsteffen@gmail.com>
2018-12-26 12:17:51 -0600

    Generalized the build script.

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -ex
 cd "$(dirname $0)"
 
+proj_name=`perl -ne '/name = "(.+)"/ && print $1' Cargo.toml | sed 's/\-/\_/g'`
+
 rustup target add wasm32-unknown-unknown --toolchain stable
 
 if ( ! command -v wasm-bindgen )
@@ -12,4 +14,4 @@ fi
 
 cargo build --target wasm32-unknown-unknown
 
-wasm-bindgen ./target/wasm32-unknown-unknown/debug/hello_webgl.wasm --out-dir ./www/wasm
+wasm-bindgen ./target/wasm32-unknown-unknown/debug/$proj_name.wasm --out-dir ./www/wasm
--- www/index.js
+++ www/index.js
@@ -1,3 +1,4 @@
+// update this file name with name in Cargo.toml
 import('./wasm/hello_webgl.js')
   .then(webgl => webgl.run())
   .catch(console.error);
